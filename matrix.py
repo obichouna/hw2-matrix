0ce92037eddb02efcddecb945c46d13fa230db23
Othman Bichouna <obichouna@stuy.edu>
2018-02-26 00:26:37 -0500

    fixed multi

--- matrix.py
+++ matrix.py
@@ -19,12 +19,12 @@ def ident(matrix):
 
 #m1 * m2 -> m2
 def matrix_mult(m1, m2):
-	ans = new_matrix(rows = len(m1[0]), cols = len(m2))
+	new = new_matrix(rows = len(m1[0]), cols = len(m2))
 	for r in range(len(m1[0])):
 		for c in range(len(m2)):
 			for i in range(len(m1)):
 				ans[r][c] += (m1[r][i] * m2[i][c])
-	return ans
+	return new
 
 
 
