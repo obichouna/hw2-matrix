7e7faf8b481b370b137ae94bd3d16d6c59a97b0e
obichouna <obichouna@stuy.edu>
2018-02-26 02:21:37 -0500

    beep boop

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from draw import *
 from matrix import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 255, 0 ]
 m1 = new_matrix()
-m2 = new_matrix()
+add_edge(m1, 100, 175, 0, 250, 100, 0)
+add_edge(m1, 400, 175, 0, 250, 100, 0)
 
-
-draw_lines(matrix, screen, color )
+draw_lines(m1, screen, color )
 display(screen)
 save_extension(screen, 'img.png')
