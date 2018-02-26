6a009a982476f40daa006e50cd56735981c96c32
Othman Bichouna <obichouna@stuy.edu>
2018-02-26 00:17:38 -0500

    Now I need to make an image

--- draw.py
+++ draw.py
@@ -3,47 +3,87 @@ from matrix import *
 
 
 def draw_lines( matrix, screen, color ):
-    pass
+    for val in range(len(matrix) / 2):
+        x0 = matrix[2 * val][0]
+        y0 = matrix[2 * val][1]
+        x1 = matrix[2 * val + 1][0]
+        y1 = matrix[2 * val + 1][1]
+        draw_line(x0, y0, x1, y1, screen, color)
 
-def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
-    pass
+def add_edge(matrix, x0, y0, z0, x1, y1, z1):
+    add_point( matrix, x0, y0, z0 )
+    add_point( matrix, x1, y1, z1 )
 
 def add_point( matrix, x, y, z=0 ):
-    pass
+    matrix.append( [x, y, z, 1] )
 
 
 
 def draw_line( x0, y0, x1, y1, screen, color ):
 
-    #swap points if going right -> left
-    if x0 > x1:
-        xt = x0
-        yt = y0
+    #This code is for when you need to flip the coordinates
+    if (x0 > x1):
+        var = x0
         x0 = x1
+        x1 = var
+        var = y0
         y0 = y1
-        x1 = xt
-        y1 = yt
+        y1 = var
 
     x = x0
     y = y0
-    A = 2 * (y1 - y0)
-    B = -2 * (x1 - x0)
+    a = y1-y0
+    b = -(x1-x0)
 
-    #octants 1
-    if ( abs(x1-x0) >= abs(y1 - y0) ):
-
-        #octant 1
-        if A > 0:
-            d = A + B/2
-
-            while x < x1:
+    if (b == 0):
+        if y0<y1:
+            while y <= y1:
                 plot(screen, color, x, y)
-                if d > 0:
-                    y+= 1
-                    d+= B
-                x+= 1
-                d+= A
-            #end octant 1 while
-            plot(screen, color, x1, y1)
-        #end octant 1
-#end draw_line
+                y += 1
+        else:
+            while y >= y1:
+                plot(screen, color, x, y)
+                y -= 1
+        return
+
+    m = -1.0 * a/b   #Slope
+
+    if m <= 1 and m >= 0:
+        d = 2 * a + b
+        while (x <= x1) :
+            plot(screen, color, x, y)
+            if d > 0:
+                y += 1
+                d += 2 * b
+            x += 1
+            d += 2 * a
+
+    elif m >= -1 and m < 0:
+        d = 2 * a - b
+        while (x <= x1) :
+            plot(screen, color, x, y)
+            if d < 0:
+                y -= 1
+                d -= 2 * b
+            x += 1
+            d += 2 * a
+
+    elif (m > 1):
+        d = a + 2 * b
+        while (y <= y1) :
+            plot(screen, color, x, y)
+            if d < 0:
+                x += 1
+                d += 2 * a
+            y += 1
+            d += 2 * b
+
+    elif (m < -1):
+        d =  a - 2 * b
+        while (y >= y1) :
+            plot(screen, color, x, y)
+            if d > 0:
+                x += 1
+                d += 2 * a
+            y -= 1
+            d -= 2 * b
--- matrix.py
+++ matrix.py
@@ -1,16 +1,35 @@
 import math
 
 
-def print_matrix( matrix ):
-    pass
+def print_matrix(matrix):
+    for i in range(len(matrix[0])):
+        row = ""
+        for x in matrix:
+           row += str(x[i]) + " "
+        print row
 
-def ident( matrix ):
-    pass
+def ident(matrix):
+    for c in range(len(matrix)):
+        for r in range(len(matrix[0])):
+            if (c == r):
+                matrix[c][r] = 1
+            else:
+                matrix[c][r] = 0
+    return matrix
 
 #m1 * m2 -> m2
-def matrix_mult( m1, m2 ):
-    pass
-
+def matrix_mult(m1, m2):
+    m = [[] for x in range(0,4)]
+    m3 = []
+    row = 0
+    for r in range(0,4):
+        for c in range(0,len(m2[0])):
+            sum = 0
+            for i in range(0,len(m1[0])):
+                prod = m1[i][r] * m2[c][i]
+                sum += prod
+            m[r].append(sum)
+    m2[:] = list(m)
 
 
 
